dc51de2a369fff09518141d622ed716c8e64c263
Alvaro Cubiella <alvarocubiella@gmail.com>
2024-07-15 03:55:18 -0300

    index modo articulos y responsivo

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,14 +1,14 @@
 let BASE_URL = 'http://localhost:5000'
 
  // Cargo la estructura del Articulo Especies, genero una copia y luego la elimino del htmo
- let modelo = document.querySelector('#tabla_modelo')
+ let modelo = document.querySelector('#art_especies')
  let modelo_copia = modelo.cloneNode(true)     // true me sirve para que lo copie con hijos incluido
  // finalmente elimino el Especies base del html
  modelo.remove()
 
 function cargar_articulos(){
     // Cargo la estructura de Main para luego poder agregar los hijos de especies
-    let tabla_especies = document.querySelector("#tabla_cuerpo")
+    let articulo_especies = document.querySelector(".contenedor_especies")
     let url = `${BASE_URL}/api/especies`
     fetchAPI(url, "GET", (error, data) => {
         // Ahora se procesa la informacion que retorna la API
@@ -17,13 +17,16 @@ function cargar_articulos(){
             // Compruebo si el articulo no tiene borrado logico
             if (especie.activo) {
                 let nueva_especie = modelo_copia.cloneNode(true)
-                nueva_especie.querySelector('#td_nombre_vulgar').innerHTML = especie.nombre_vulgar;
-                nueva_especie.querySelector('#td_nombre_cientifico').innerHTML = especie.nombre_cientifico;
+                nueva_especie.querySelector('#nombre_vulgar').innerHTML = especie.nombre_vulgar;
+                nueva_especie.querySelector('#nombre_cientifico').innerHTML = `(${especie.nombre_cientifico})`;
+                nueva_especie.querySelector('#hdr_temporada').innerHTML = "Temporada de Pesca";
+                nueva_especie.querySelector('#epoca').innerHTML = especie.epoca;
                 let btn_ver = nueva_especie.querySelector('#view_button');
                 btn_ver.onclick = () => viewSpecies(especie)
                 let btn_del = nueva_especie.querySelector('#del_button');
                 btn_del.onclick = () => deleteSpecies(especie)
-                tabla_especies.appendChild(nueva_especie)
+                // Cargo el nuevo articulo al main
+                articulo_especies.appendChild(nueva_especie)
             };
         };
     });
